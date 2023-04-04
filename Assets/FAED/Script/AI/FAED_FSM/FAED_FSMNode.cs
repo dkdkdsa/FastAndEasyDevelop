46b453dd65a9eda600f2cb855e7d9b7378913c53
dkdkdsa <bos94330@gmail.com>
2023-04-05 08:39:01 +0900

    FAED_FSMType

---
+++ Assets/FAED/Script/AI/FAED_FSM/FAED_FSMNode.cs
@@ -0,0 +1,14 @@
+#if UNITY_EDITOR
+using FD.AI.Node;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class FAED_FSMNode : Node
+{
+
+    public string GUID;
+
+}
+#endif
