9ecf1c2dccf6c15d8cd588eb8bd1ae0ea00a0c61
dkdkdsa <bos94330@gmail.com>
2023-04-07 17:50:57 +0900

    Update FAED_FSMGraphViewWindow.cs

--- Assets/FAED/Script/AI/FAED_FSM/FAED_FSMGraphViewWindow.cs
+++ Assets/FAED/Script/AI/FAED_FSM/FAED_FSMGraphViewWindow.cs
@@ -2,6 +2,7 @@
 using FD.UI.Tool;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace FD.AI.FSM
@@ -10,7 +11,37 @@ namespace FD.AI.FSM
     public class FAED_FSMGraphViewWindow : FAED_GraphViewTool<FAED_FSMGraph>
     {
 
+        [MenuItem("FAED_AI/FAED_FSM")]
+        private static void CreateWindow()
+        {
 
+            var window = GetWindow<FAED_FSMGraphViewWindow>();
+            window.titleContent = new GUIContent("FAED_FSM");
+
+        }
+
+        private void OnEnable()
+        {
+
+            StartSetting();
+
+        }
+
+        private void OnDisable()
+        {
+
+            rootVisualElement.Remove(graphView);
+            graphView = null;
+
+        }
+
+        private void StartSetting()
+        {
+
+            CreateWindowElement("FAED_FSM", true, true);
+            toolbar.AddTextField("FileName");
+
+        }
 
     }
 
