4efc731d67b00065390fd18cc3f1506cf858ab0c
dkdkdsa <bos94330@gmail.com>
2023-04-08 19:04:05 +0900

    FAED_Core

---
+++ Assets/FAED/Manager/Core/FAED_Core.cs
@@ -0,0 +1,25 @@
+using FD.System.Manager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FD.System.Core
+{
+
+    public class FAED_Core : MonoBehaviour
+    {
+
+        private static FAED_PoolManager poolManager;
+
+        public static FAED_PoolManager PoolManager { get { return poolManager; } }
+
+        private static void Init()
+        {
+
+
+
+        }
+
+    }
+
+}
