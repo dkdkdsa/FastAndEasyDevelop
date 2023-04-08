a3ad97213a7d7c8c2207dd46ae99105f11656c19
dkdkdsa <bos94330@gmail.com>
2023-04-09 01:05:39 +0900

    FAED_DelayInvoke

--- Assets/FAED/Manager/Delay/FAED_DelayInvoke.cs
+++ Assets/FAED/Manager/Delay/FAED_DelayInvoke.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,35 @@ namespace FD.System.Delay
     public class FAED_DelayInvoke : MonoBehaviour
     {
 
+        public void InvokeDelay(Action action, float delSec)
+        {
 
+            StartCoroutine(DelayCo(action, delSec));
+
+        }
+
+        public void InvokeDelayReal(Action action, float delSec)
+        {
+
+            StartCoroutine(RealDelayCo(action, delSec));
+
+        }
+
+        IEnumerator DelayCo(Action action, float delSec)
+        {
+
+            yield return new WaitForSeconds(delSec);
+            action?.Invoke();
+
+        }
+
+        IEnumerator RealDelayCo(Action action, float delSec)
+        {
+
+            yield return new WaitForSecondsRealtime(delSec);
+            action?.Invoke();
+
+        }
 
     }
 
