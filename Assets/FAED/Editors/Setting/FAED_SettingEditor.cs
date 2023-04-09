2e897e50275748c9ccdc969da21e47ac4d7d14ad
dkdkdsa <bos94330@gmail.com>
2023-04-10 00:53:10 +0900

    FAED_SettingEditor

--- Assets/FAED/Editors/Setting/FAED_SettingEditor.cs
+++ Assets/FAED/Editors/Setting/FAED_SettingEditor.cs
@@ -1,16 +1,33 @@
 #if UNITY_EDITOR
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class FAED_SettingEditor : EditorWindow
 {
 
+    [MenuItem("Test/Testing")]
     public static void CreateSettingWindow()
     {
 
         var window = GetWindow<FAED_SettingEditor>();
+
+    }
+
+    private void OnEnable()
+    {
+
+        var image = new Image();
+        Texture2D texture = new Texture2D(0, 0);
+        texture.LoadImage(File.ReadAllBytes(AppDomain.CurrentDomain.BaseDirectory + "Image/FAED.png"));
+
+        image.image = texture;
+        image.sourceRect = new Rect(0, 0, 100, 100);
+        rootVisualElement.Add(image);
 
     }
 
