11bc926c0b20da607546f66002efecad596f23b6
dkdkdsa <bos94330@gmail.com>
2023-04-10 13:40:35 +0900

    FAED_SettingEditor

--- Assets/FAED/Editors/Setting/FAED_SettingEditor.cs
+++ Assets/FAED/Editors/Setting/FAED_SettingEditor.cs
@@ -15,6 +15,9 @@ public class FAED_SettingEditor : EditorWindow
     {
 
         var window = GetWindow<FAED_SettingEditor>();
+        window.minSize = new Vector2(300, 500);
+        window.maxSize = window.minSize;
+        window.Show();
 
     }
 
@@ -22,11 +25,13 @@ public class FAED_SettingEditor : EditorWindow
     {
 
         var image = new Image();
-        Texture2D texture = new Texture2D(0, 0);
-        texture.LoadImage(File.ReadAllBytes(AppDomain.CurrentDomain.BaseDirectory + "Image/FAED_Logo.png"));
 
+        Texture2D texture = Resources.Load<Texture2D>("FAED/Image/FAED_Logo");
+        
         image.image = texture;
-        image.sourceRect = new Rect(0, 0, 100, 100);
+        image.style.flexShrink = 100;
+        image.style.flexGrow = 0.3f;
+
         rootVisualElement.Add(image);
 
     }
