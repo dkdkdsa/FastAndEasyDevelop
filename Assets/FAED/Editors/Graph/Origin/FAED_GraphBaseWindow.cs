72c37ce2ebd1d5d6a92313450f1cceee1e8c0202
dkdkdsa <bos94330@gmail.com>
2023-04-11 13:49:10 +0900

    FAED_GraphBaseWindow

---
+++ Assets/FAED/Editors/Graph/Origin/FAED_GraphBaseWindow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FD.System.Editor.Graph
+{
+
+    public class FAED_GraphBaseWindow : MonoBehaviour
+    {
+        
+
+
+    }
+
+}
