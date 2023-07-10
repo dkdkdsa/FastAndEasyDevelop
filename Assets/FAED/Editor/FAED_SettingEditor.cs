6d2c04432ef86d48fa992b99bc3d11dd37aa33ad
dkdkdsa <bos94330@gmail.com>
2023-07-10 12:44:26 +0900

    FAED_SettingEditor

--- Assets/FAED/Editor/FAED_SettingEditor.cs
+++ Assets/FAED/Editor/FAED_SettingEditor.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -18,14 +19,79 @@ namespace FD.Core.Editors
             window.titleContent.text = "FAED_Setting";
             window.maxSize = new Vector2(300, 500);
             window.minSize = new Vector2(300, 500);
+            window.SettingWindow();
             window.Show();
 
         }
 
+        
+
         private void SettingWindow()
         {
+            var res = Resources.Load<FAED_SettingSO>("FAED/SettingSO");
 
+            if (res == null)
+            {
+                if (!Directory.Exists(Application.dataPath + "/Resources"))
+                {
 
+                    Directory.CreateDirectory(Application.dataPath + "/Resources");
+
+                }
+
+                if (!Directory.Exists(Application.dataPath + "/Resources/FAED"))
+                {
+
+                    Directory.CreateDirectory(Application.dataPath + "/Resources/FAED");
+
+                }
+
+                var obj = CreateInstance<FAED_SettingSO>();
+                AssetDatabase.CreateAsset(obj, "Assets/Resources/FAED/SettingSO.Asset");
+                res = Resources.Load<FAED_SettingSO>("FAED/SettingSO");
+
+                AssetDatabase.Refresh();
+
+            }
+
+            var image = new Image();
+
+            Texture2D texture = Resources.Load<Texture2D>("FAED_Logo");
+
+            image.image = texture;
+            image.style.flexShrink = 100;
+            image.style.flexGrow = 0.3f;
+
+            Label label = new Label("FAED_Setting");
+            Toggle toggle = new Toggle("UsePooling");
+            Button settingCompleteButton = new Button(() =>
+            {
+
+                var so = Resources.Load<FAED_SettingSO>("FAED/SettingSO");
+
+                so.usePooling = toggle.value;
+
+                if (so.usePooling && Resources.Load<FAED_PoolingSO>("FAED/PoolingSO") == null)
+                {
+
+                    var obj = CreateInstance<FAED_PoolingSO>();
+                    AssetDatabase.CreateAsset(obj, "Assets/Resources/FAED/PoolingSO.Asset");
+                    res.poolingSO = Resources.Load<FAED_PoolingSO>("FAED/PoolingSO");
+
+                }
+
+                Close();
+
+            });
+
+            settingCompleteButton.text = "SettingComplete";
+
+            toggle.value = res.usePooling;
+
+            rootVisualElement.Add(image);
+            rootVisualElement.Add(label);
+            rootVisualElement.Add(toggle);
+            rootVisualElement.Add(settingCompleteButton);
 
         }
 
